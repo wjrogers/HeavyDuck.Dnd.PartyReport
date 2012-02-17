2e526c6ef26e137cb51b8f71d8c05f218a062e7b
Will Rogers <wrogers@xo-energy.com>
2012-02-17 10:27:07 -0500

    Make it do stuff.

--- HeavyDuck.Dnd.PartyReport/MainForm.Designer.cs
+++ HeavyDuck.Dnd.PartyReport/MainForm.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.IntegralHeight = false;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(660, 480);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(597, 527);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 562);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- HeavyDuck.Dnd.PartyReport/MainForm.cs
+++ HeavyDuck.Dnd.PartyReport/MainForm.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
 
 namespace HeavyDuck.Dnd.PartyReport
 {
@@ -14,6 +18,126 @@ namespace HeavyDuck.Dnd.PartyReport
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                if (DialogResult.OK == dialog.ShowDialog(this))
+                    listBox1.Items.Add(dialog.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var items = new Dictionary<string, List<MagicItem>>();
+            var output = @"C:\Temp\party.html";
+
+            foreach (string path in listBox1.Items)
+            {
+                using (var fs = File.OpenRead(path))
+                {
+                    XPathDocument doc;
+                    XPathNavigator nav;
+
+                    doc = new XPathDocument(fs);
+                    nav = doc.CreateNavigator();
+
+                    var character_name = nav.SelectSingleNode("/D20Character/CharacterSheet/Details/name").Value.Trim();
+                    var inventory = new List<MagicItem>();
+
+                    for (var iter = nav.Select("//LootTally/loot[@count > 0]/RulesElement[@type = 'Magic Item']"); iter.MoveNext(); )
+                    {
+                        inventory.Add(new MagicItem
+                        {
+                            Name = iter.Current.SelectSingleNode("@name").Value.Trim(),
+                            Url = iter.Current.SelectSingleNode("@url").Value.Trim(),
+                            Level = iter.Current.SelectSingleNode("specific[@name = 'Level']").ValueAsInt,
+                            MagicItemType = iter.Current.SelectSingleNode("specific[@name = 'Magic Item Type']").Value.Trim(),
+                        });
+                    }
+
+                    items[character_name] = inventory;
+                }
+            }
+
+            using (var fs = File.Open(output, FileMode.Create, FileAccess.Write))
+            {
+                using (var writer = XmlWriter.Create(fs))
+                {
+                    writer.WriteStartElement("html");
+                    writer.WriteStartElement("body");
+
+                    foreach (var character in items)
+                    {
+                        writer.WriteStartElement("div");
+                        writer.WriteAttributeString("style", "float: left;");
+
+                        writer.WriteElementString("h1", character.Key);
+
+                        var item_lookup = character.Value.ToLookup(g => g.MagicItemType);
+                        var items_remaining = character.Value.ToList();
+
+                        writer.WriteElementString("h2", "Weapons");
+                        writer.WriteStartElement("ul");
+                        foreach (var item in item_lookup["Weapon"].OrderBy(i => i.Name))
+                        {
+                            writer.WriteStartElement("li");
+                            writer.WriteStartElement("a");
+                            writer.WriteAttributeString("href", item.Url);
+                            writer.WriteValue(string.Format("L{0:00} {1}", item.Level, item.Name));
+                            writer.WriteEndElement(); // a
+                            writer.WriteEndElement(); // li
+
+                            items_remaining.Remove(item);
+                        }
+                        writer.WriteEndElement(); // ul
+
+                        writer.WriteElementString("h2", "Armor");
+                        writer.WriteStartElement("ul");
+                        foreach (var item in item_lookup["Armor"].Concat(item_lookup["Neck Slot Item"]).OrderBy(i => Tuple.Create(i.MagicItemType, i.Name)))
+                        {
+                            writer.WriteStartElement("li");
+                            writer.WriteStartElement("a");
+                            writer.WriteAttributeString("href", item.Url);
+                            writer.WriteValue(string.Format("L{0:00} {1}", item.Level, item.Name));
+                            writer.WriteEndElement(); // a
+                            writer.WriteEndElement(); // li
+
+                            items_remaining.Remove(item);
+                        }
+                        writer.WriteEndElement(); // ul
+
+                        writer.WriteElementString("h2", "Other");
+                        writer.WriteStartElement("ul");
+                        foreach (var item in items_remaining.OrderByDescending(i => i.Level))
+                        {
+                            writer.WriteStartElement("li");
+                            writer.WriteStartElement("a");
+                            writer.WriteAttributeString("href", item.Url);
+                            writer.WriteValue(string.Format("L{0:00} {1}", item.Level, item.Name));
+                            writer.WriteEndElement(); // a
+                            writer.WriteEndElement(); // li
+                        }
+                        writer.WriteEndElement(); // ul
+
+                        writer.WriteEndElement(); //div
+                    }
+
+                    writer.WriteEndDocument();
+                }
+            }
+
+            Process.Start(output);
+        }
+
+        private class MagicItem
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+            public int Level { get; set; }
+            public string MagicItemType { get; set; }
         }
     }
 }
